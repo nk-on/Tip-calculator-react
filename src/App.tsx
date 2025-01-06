303d5a32b1248feed0d736741f864e17cf390939
nk-on <johndoe@example.com>
2025-01-06 22:13:34 +0400

    Adding grid system on button sections

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,27 @@ import viteLogo from '/vite.svg';
 import './App.css';
 import Form from './components/Form';
 import { PercentageButton } from './components/PercentageButton';
-import  buttons from './buttons'
+import buttons from './buttons';
 function App() {
   return (
     <>
       <div className="bg-[#FFFFFF] w-[90%] h-[481px] rounded-[25px] md:w-[920px]">
-        <div className="w-[50%] h-[100%] border border-red-500">
-          <Form title={"Bill"}/>
-          <div>
-             {buttons.map((buttonObject)=>{
-              return <PercentageButton key = {buttonObject.id} percentageRate={buttonObject.rate}/>
-             })}
+        <div className="w-[50%] h-[100%] flex flex-col justify-evenly	 border border-red-500 pl-[25px]">
+          <Form title={'Bill'} />
+          <div className="flex flex-col ">
+            Select tip
+            <div className='grid grid-cols-3 gap-[5px]'>
+              {buttons.map((buttonObject) => {
+                return (
+                  <PercentageButton
+                    key={buttonObject.id}
+                    percentageRate={buttonObject.rate}
+                  />
+                );
+              })}
+            </div>
           </div>
-          <Form title={"Number of People"}/>
+          <Form title={'Number of People'} />
         </div>
       </div>
     </>
