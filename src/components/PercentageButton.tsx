8e7f886d22ef31329adbb9831ff0dd124b2e8aec
nk-on <2nkoz@proton.me>
2025-01-09 22:09:38 -0800

    Feat:adding error checking system when making custom percentage input

--- src/components/PercentageButton.tsx
+++ src/components/PercentageButton.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 interface percentageProps {
   percentageRate: number;
   isCustom: boolean;
@@ -10,6 +12,7 @@ export function PercentageButton({
   percentage,
   setPercentage,
 }: percentageProps) {
+  const [error,setError] = useState(false);
   /* 1. when user clicks on percentage rate it should be green untill user clicks on another button and this other button
     should become green
     2.add all buttons in button object isClicked false if id === button propsid then change it to trues
@@ -20,9 +23,17 @@ export function PercentageButton({
         <input
           type="number"
           placeholder="Custom"
-          className="w-[117px] h-[48px] text-[#547878] rounded-[5px] text-[24px] bg-[#F3F9FA] font-bolder"
+          className={`w-[117px] h-[48px] text-[#547878] rounded-[5px] text-[24px] bg-[#F3F9FA] font-bolder ${
+            error && "border border-red-500"
+          }`}
           onChange={(event) => {
             const value = Number(event.target.value);
+            if(value <= 0){
+              setError(true);
+              event.target.value = ''
+              return;
+            }
+            setError(false);
             setPercentage(value);
           }}
         />
