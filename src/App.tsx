ae6caf3b640db237a8ff10fa4b374336ccef1dd7
nk-on <2nkoz@proton.me>
2025-01-09 21:33:44 -0800

    Fix:fixing bug in particular App was not calculating total amount when making iinputs i fixed this issue

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ import Form from "./components/Form";
 import DataContainer from "./components/Data";
 import { PercentageButton } from "./components/PercentageButton";
 import buttons from "./buttons";
-interface buttonObjectProp{
-  id:number,
-  rate:number,
-  isClicked:boolean,
-  isCustom:boolean,
+interface buttonObjectProp {
+  id: number;
+  rate: number;
+  isClicked: boolean;
+  isCustom: boolean;
 }
 type buttonType = buttonObjectProp[];
 function App() {
@@ -19,8 +19,7 @@ function App() {
   const [bill, setBill] = useState<number>(0);
   const [amountOfPeople, setAmountOfPeople] = useState<number>(0);
   const [percentage, setPercentage] = useState<number>(0);
-  const billProps = {
-    title: "Bill",
+  const formProps = {
     setBill,
     setAmountOfPeople,
   };
@@ -28,33 +27,24 @@ function App() {
     <>
       <div className="flex flex-col md:flex-row bg-[#FFFFFF] w-[90%] min-h-[481px] md:h-[481px] rounded-[25px] md:w-[920px]">
         <div className="w-[90%] h-[100%] md:w-[50%] flex flex-col justify-evenly  pl-[25px]">
-          <Form
-            {...billProps}
-          />
+          <Form {...{ title: "Bill", ...formProps }} />
           <div className="flex flex-col ">
             <div className="text-[#5E7A7D] font-bold">Select tip</div>
             <div className="grid grid-cols-2 md:grid-cols-3 gap-[5px]">
               {buttonsObj.map((buttonObject) => {
                 const buttonProps = {
-                  key: buttonObject.id,
                   percentageRate: buttonObject.rate,
                   id: buttonObject.id,
                   isClicked: buttonObject.isClicked,
                   isCustom: buttonObject.isCustom,
                   setButtonsObj,
                   setPercentage,
-                };  
-                return (
-                  <PercentageButton
-                    {...buttonProps}
-                  />
-                );
+                };
+                return <PercentageButton key={buttonObject.id} {...buttonProps} />;
               })}
             </div>
           </div>
-          <Form
-            {...billProps}
-          />
+          <Form {...{ title: "amount of people", ...formProps }} />
         </div>
         <div className=" flex justify-center items-center w-[90%] md:w-[50%] h-[100%]">
           <div className="flex flex-col items-center justify-evenly bg-[#00474B] w-[90%] h-[90%] rounded-[25px] px-[25px]">
@@ -66,7 +56,12 @@ function App() {
               title="Total Amount"
               amount={((bill * percentage) / 100) * amountOfPeople}
             />
-            <Reset setBill={setBill} setPercentage={setPercentage} setAmountOfPeople={setAmountOfPeople} setButtonsObj = {setButtonsObj}/>
+            <Reset
+              setBill={setBill}
+              setPercentage={setPercentage}
+              setAmountOfPeople={setAmountOfPeople}
+              setButtonsObj={setButtonsObj}
+            />
           </div>
         </div>
       </div>
